a214050ced9b96da33e1efd5dd5fe5432803afd4
raoniee <126031282+raoniee@users.noreply.github.com>
2023-03-08 13:42:27 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,66 @@
+const items = document.querySelector('.items');
+const form = document.querySelector('.new-form');
+const input = document.querySelector('.footer_input');
+const addBtn = document.querySelector('.footer_button');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    onAdd();
+})
+
+function onAdd() {
+    //1. 사용자가 입력한 텍스트를 받아옴
+    const text = input.value;
+    if (text === '') {
+        input.focus();
+        return;
+    };
+    //2. 새로운 아이템을 만듦(텍스트+삭제버튼)
+    const item = createItem(text);
+    //3. items 컨테이너안에 새로 만든 아이템을 추가한다.
+    items.appendChild(item);
+    //4. 새로 추가된 아이템으로 스크롤링
+    item.scrollIntoView({ block: "center" });
+    //5. 인풋을 초기화 한다.
+    input.value = '';
+    input.focus();
+}
+
+let id = 0; //UUID
+function createItem(text) {
+    const itemRow = document.createElement('li');
+    itemRow.setAttribute('class', 'item_row');
+    itemRow.setAttribute('data-id', id);
+    itemRow.innerHTML = `
+        <div class="item">
+            <span class="item_name">${text}</span>
+            <button class="item_delete">
+                <i class="xi-trash" data-id=${id}></i>
+            </button>
+        </div>
+        <div class="item_divider"></div>
+    `;
+    id++;
+    return itemRow;
+}
+
+// addBtn.addEventListener('click', () => {
+//     onAdd();
+// });
+
+// input.addEventListener('keydown', event => {
+//     if (event.isComposing) {
+//         return;
+//     }
+//     if (event.key === 'Enter') {
+//         onAdd();
+//     }
+// });
+
+items.addEventListener('click', event => {
+    const id = event.target.dataset.id;
+    if (id) {
+        const toBeDeleted = document.querySelector(`.item_row[data-id="${id}"]`);
+        toBeDeleted.remove();
+    }
+})
